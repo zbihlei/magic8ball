62804b2534c4f1a49d40698578285e0e1c970b30
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-21 11:45:39 +0200

    delete shake / add space event

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -3,19 +3,18 @@ import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/main.module.scss';
 import useRandomAnswer from '../hooks/useRandom';
-import useShakeDetection from '../hooks/useShakeDetection';
 
 function Main() {
-  const [clicked, setClicked] = useState(false);
-  const [isLogoShifted, setIsLogoShifted] = useState(false);
-  const [isTriangleShifted, setTriangleShifted] = useState(false);
-  const [text, setShowText] = useState(false);
-  const [answer, setAnswer] = useState('');
+  const [clicked, setClicked] = useState<boolean>(false);
+  const [isLogoShifted, setIsLogoShifted] = useState<boolean>(false);
+  const [isTriangleShifted, setTriangleShifted] = useState<boolean>(false);
+  const [text, setShowText] = useState<boolean>(false);
+  const [answer, setAnswer] = useState<string>('');
   const getRandomAnswer = useRandomAnswer();
 
   useEffect(()=>{
     setAnswer(getRandomAnswer());
-  },[text]);
+  },[text]); 
 
  const handleToggleClass = () => {
   setClicked(true);
@@ -28,8 +27,15 @@ function Main() {
   },1000)
   };
 
-  useShakeDetection(handleToggleClass, { threshold: 15 });
-
+  const handleSpaceKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.code === 'Space') {
+      if (clicked) {
+        handleResetStyles();
+      } else {
+        handleToggleClass();
+      }
+    }
+  };
   const handleResetStyles = () => {
     setIsLogoShifted(false);
     setTriangleShifted(false);
@@ -40,7 +46,7 @@ function Main() {
   };
 
   return (
-    <div className={styles.wrapp}>
+    <div className={styles.wrapp} onKeyDown={handleSpaceKeyPress} tabIndex={0} role="button">
       <div className={styles.stars}></div>
       <div className={styles.twinkling}></div>
         <div className={`${styles.ball} ${clicked ? styles.shakeAnimation : ''}`}>
